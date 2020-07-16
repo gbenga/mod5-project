4c4bd40a5ef0decea465bc3ef2b523468596abf0
gojoaromokudu <gojoaromokudu@gmail.com>
2020-07-16 18:55:07 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
 import "./App.css";
 import MedicinesIndexPage from "./components/pages/MedicinesIndexPage";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  withRouter,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <MedicinesIndexPage />
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/medicines">Medicines</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/medicines">
+            <MedicinesIndexPage />
+          </Route>
+          <Route exact path="/">
+            <div>Just a placeholder for a homepage</div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
